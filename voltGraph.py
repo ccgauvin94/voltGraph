282ad49687a30774af4b3622fbabe172dd2f7830
Colin Gauvin <ccgauvin94@gmail.com>
2020-01-02 08:38:50 -0700

    Update UI

--- voltGraph.py
+++ voltGraph.py
@@ -1,26 +1,62 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QToolBar, QToolButton
 import pyqtgraph as pg
 import voltageEmulator
-import subprocess
 
-# Initialize Qt
-voltGraph = QApplication([])
-mainWindow = QWidget()
-start = QPushButton('Start')
-graph = pg.PlotWidget()
+# Define global variables/settings
+emulator = True
 
-# Grid layout to manage display
-layout = QGridLayout()
-mainWindow.setLayout(layout)
-layout.addWidget(start)
-layout.addWidget(graph)
 
-# Graph Loop
-timePassed = 0.0
-coordinates = []
-while True:
-    coordinates.append(voltageEmulator.emulate())
+def ui():
+    # Initialize Qt
+    app = QApplication([])
 
-# Emulate voltage input
-mainWindow.show()
-voltGraph.exec_()
+    # Initialize main window
+    window = QWidget()
+    window.setWindowTitle("voltGraph v0.0.1")
+    layout = QGridLayout()
+    window.setLayout(layout)
+
+    # Initialize toolbar
+    menu = QToolBar
+    file = QToolButton()
+    file.setText('File')
+    edit = QToolButton()
+    edit.setText('Edit')
+
+    # Initialize buttons
+    start = QPushButton('Start')
+    stop = QPushButton('Stop')
+    stop.setDisabled(True)
+    save = QPushButton('Save')
+    save.setDisabled(True)
+    load = QPushButton('Load')
+
+    # Initialize blank graph
+    plot = pg.PlotWidget()
+
+    # Grid layout to manage display
+    layout.addWidget(start)
+    layout.addWidget(stop)
+    layout.addWidget(save)
+    layout.addWidget(load)
+    layout.addWidget(plot, 2, 2, 3, 3)
+
+    # Show main UI
+    window.show()
+    app.exec_()
+
+
+def graph():
+    # Import emulator setting
+    global emulator
+
+    # Initialize the coordinates variable
+    coordinates = []
+
+    # If using emulator
+    if emulator:
+        while True:
+            coordinates.append(voltageEmulator.emulate())
+
+
+ui()
