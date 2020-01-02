e26b5e288425913719d632f6ee3e7004567c8867
Colin Gauvin <ccgauvin94@gmail.com>
2020-01-01 20:51:05 -0700

    Update emulator

--- voltGraph.py
+++ voltGraph.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout
 import pyqtgraph as pg
 import voltageEmulator
-import time
+import subprocess
 
 ##Initialize Qt
 voltGraph = QApplication([])
@@ -19,11 +19,7 @@ layout.addWidget(graph, 0, 1, 3, 1)
 timePassed = 0.0
 coordinates = []
 while True:
-    readout = [timePassed, voltageEmulator.emulate()]
-    timePassed = timePassed + 0.1
-    coordinates.append(readout)
-    time.sleep(0.1)
-
+    coordinates.append(voltageEmulator.emulate())
 
 #Emulate voltage input
 mainWindow.show()
--- voltageEmulator.py
+++ voltageEmulator.py
@@ -1,7 +1,12 @@
 import random
+import time
+
+timePassed = 0.0
+
 
 def emulate():
     voltage = random.uniform(-5, 5)
-    return (voltage)
-
-
+    global timePassed
+    timePassed = timePassed + 0.1
+    time.sleep(0.1)
+    return [timePassed, voltage]
